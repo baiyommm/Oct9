7bc0e9f3d0ecf3f780f4f6583af2e52f0a77ff4e
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-10-14 09:40:37 -0700

    first update. program starts coding

--- Oct9/main.cpp
+++ Oct9/main.cpp
@@ -1,14 +1,20 @@
-//
-//  main.cpp
-//  Oct9
-//
-//  Created by Chatchapong Thuaypha on 10/9/24.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// using pointers to reverse the variables in the array
+
+int main() {
+    
+    int numbers[] = {10, 20, 30, 40, 50};
+
+    int* ptr = numbers + size(numbers) - 1;
+
+    for (int i = 0; i < size(numbers); i++) {
+        cout << *ptr << " ";
+        ptr--;
+    }
+
+    cout << endl;
+    
     return 0;
 }
